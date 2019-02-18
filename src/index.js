ee99455ee72c556eb0f0ce847f1aafb5804cfdeb
Bogdan <bogdan.pashynskyi@gmail.com>
2019-02-18 21:53:19 +0200

    css loader was added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const title = 'TicTacToe!';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <div className="title">{title}</div>,
   document.getElementById('app')
 );
 
