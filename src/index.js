297b7c21035fe3cc578df380addd5ae0c76e0508
Bogdan <bogdan.pashynskyi@gmail.com>
2019-02-18 13:52:55 +0200

    hot loader installed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'TicTacToe';
+const title = 'TicTacToe!';
 
 ReactDOM.render(
   <div>{title}</div>,
   document.getElementById('app')
 );
+
